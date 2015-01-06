12c78ecaa89c09e0a6b690d3219794b66c94fff6
Taras Sereda <taras.y.sereda@gmail.com>
2015-01-06 17:22:00 +0200

    weak_point improved

--- weak_point.py
+++ weak_point.py
@@ -1,15 +1,11 @@
 __author__ = 'taras-sereda'
 
-
 def weak_point(matrix):
-
     cols = map(sum,zip(*matrix))
-    print type(cols)
-    rows = [sum(line) for line in matrix]
+    rows = map(sum,matrix)
     week_col = cols.index(min(cols))
     week_row = rows.index(min(rows))
-    return [week_row,week_col]
-
+    return week_row,week_col
 
 import numpy as np
 
@@ -21,8 +17,16 @@ def weak_point_numpy(matrix):
     week_col = sum_cols.index(min(sum_cols))
     return [week_row,week_col]
 
-
+# for learning purpose
+# @Sim0000 on checkio
+# def weak_point(matrix):
+#     n = len(matrix)
+#     row = min(range(n), key=lambda r:sum(matrix[r][c] for c in range(n)))
+#     col = min(range(n), key=lambda c:sum(matrix[r][c] for r in range(n)))
+#     return row, col
 if __name__ == '__main__':
+
+
 
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert isinstance(weak_point([[1]]), (list, tuple)), "The result should be a list or a tuple"
