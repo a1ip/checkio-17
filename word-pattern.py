03ebe00ac0a17587ecc4ab63f49c6b37779d0240
Taras Sereda <taras.y.sereda@gmail.com>
2015-01-24 11:12:01 +0200

    new tasks solved

---
+++ word-pattern.py
@@ -0,0 +1,24 @@
+import string
+def check_command(pattern, command):
+    seq_size = len(command)
+    pattern = format(pattern, '0{0}b'.format(seq_size))
+    if len(pattern) != len(command): return False
+    formated_command = map(lambda x: '1' if x not in string.digits else '0', command)
+    formated_command = "".join(formated_command)
+
+    res = [i==j for i,j in zip(formated_command,pattern)]
+    if False in res:
+        return False
+    else:
+        return True
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_command(42, u"12a0b3e4") == True, "42 is the answer"
+    assert check_command(101, u"ab23b4zz") == False, "one hundred plus one"
+    assert check_command(0, u"478103487120470129") == True, "Any number"
+    assert check_command(127, u"Checkio") == True, "Uppercase"
+    assert check_command(7, u"Hello") == False, "Only full match"
+    assert check_command(8, u"a") == False, "Too short command"
+    assert check_command(5, u"H2O") == True, "Water"
+    assert check_command(42, u"C2H5OH") == False, "Yep, this is not the Answer"
