09b32ac0ea5e603d0f974533ed32020f6ffdd6f7
Taras Sereda <taras.y.sereda@gmail.com>
2015-04-02 00:17:54 +0300

    new task solved

--- dna-split-sort.py
+++ dna-split-sort.py
@@ -1,14 +1,1 @@
-def golf(s,p):
-    l=map(''.join,zip(*[iter(s)]*p))
-    d=[]
-    for e in l:
-        l_s=len(e)
-        c=0
-        for i in range(l_s):
-            for j in range(i,l_s):
-                if ord(e[i])>ord(e[j]):
-                    c+=1
-        d.append((c,e))
-    s_d=sorted(d,key=lambda t:t[0])
-    l=[i[1] for i in s_d]
-    return ''.join(l)
+golf=lambda s,p:''.join(v[1]for v in sorted([(sum(e[i]>e[j]for i in range(len(e))for j in range(i,len(e))),e)for e in map(''.join,zip(*[iter(s)]*p))],key=lambda t:t[0]))
