87c5085a6b3a752da8cb60bd2e61621e96d84bd2
Taras Sereda <taras.y.sereda@gmail.com>
2015-04-02 21:36:52 +0300

    new task solved

---
+++ most-wanted-letter.py
@@ -0,0 +1,15 @@
+import string
+from collections import Counter
+checkio=lambda text:sorted(Counter(filter(lambda x: x in string.ascii_lowercase, text.lower())).most_common(),key=lambda x:(x[1],-ord(x[0])))[-1][0]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(u"Hello World!") == "l", "Hello test"
+    assert checkio(u"How do you do?") == "o", "O is most wanted"
+    assert checkio(u"One") == "e", "All letter only once."
+    assert checkio(u"Oops!") == "o", "Don't forget about lower case."
+    assert checkio(u"AAaooo!!!!") == "a", "Only letters."
+    assert checkio(u"abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio(u"a" * 9000 + u"b" * 1000) == "a", "Long."
+    print("The local tests are done.")
